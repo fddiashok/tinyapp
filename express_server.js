ee75ebbd7eec5ac54a21ec27b03d472cd18192e0
fddiashok <fddiashok@gmail.com>
2021-12-08 21:48:42 -0500

    implemented edit and delete functionlaity

--- express_server.js
+++ express_server.js
@@ -31,7 +31,7 @@ app.get('/urls/:shortURL', (req, res) => {
     shortURL,
     longURL
   };
-res.render('urls_show',templateVars); // render and pass object to urls_show template
+  res.render('urls_show', templateVars); // render and pass object to urls_show template
 });
 
 app.get('/u/:shortURL', (req, res) => {
@@ -39,7 +39,7 @@ app.get('/u/:shortURL', (req, res) => {
   // console.log(shortURL);
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
-  });
+});
 
 
 app.post('/urls', (req, res) => {
@@ -51,12 +51,19 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${shortURL}`)       // Redirect to /urls/randomid
 });
 
-app.post('/urls/:shortURL/delete', (req,res) => {
-const shortURL = req.params.shortURL;
-delete urlDatabase[shortURL];
-res.redirect('/urls');
+app.post('/urls/:shortURL/delete', (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect('/urls');
 })
 
+app.post('/urls/:shortURL', (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL]= longURL;
+  res.redirect('/urls');
+  
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
